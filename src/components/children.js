51486b91bce40d1c56914779a0a5e3d8423c0224
stevelagarto <stevenzgz@gmail.com>
2020-01-09 21:17:30 +0100

    request working, some bugss

--- src/components/children.js
+++ src/components/children.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Children() {
-  return (<div>CHILDREN</div>)
+function Children({ data }) {
+  // return (<div className="children"><img width="150px" height="100px" src={data.userImageURL} /></div>)
+  return (<div>{data.likes}</div>)
 
  }
-
 
 export default Children;
--- src/components/infiniteScroll.js
+++ src/components/infiniteScroll.js
@@ -2,50 +2,57 @@ import React, {useRef, useEffect, useState} from 'react';
 import Item from './lazyLoadItem';
 import Children from './children';
 
+let pages = 1
 const request = async () => {
-  const response = await fetch('https://pixabay.com/api/?key=12607862-fd9c4e5d2ce0e316e5fe18d32&q=yellow+flowers&image_type=photo');
+  const response = await fetch('https://pixabay.com/api/?key=12607862-fd9c4e5d2ce0e316e5fe18d32&image_type=photo&per_page=50&page=' + pages);
   const json = await response.json();
-  console.log(json);
+  console.log('FETCHED', json);
+  
+  return(json);
 }
 
 
 
 
 let counter = 0;
-const initialData = Array(50).fill();
+const initialData = Array(50).fill('mock');
 IntersectionObserver.prototype.POLL_INTERVAL = 100;
 
 const InfiniteScroll = ( { rootVal = null, rootMargin = '50px', threshold= 1.0 }) => {
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState([]);
   const loader = useRef(null);
-  
+
   useEffect(() => {
-  
-  request(); 
-  const options = { // options on props
-    root: rootVal,
-    rootMargin: rootMargin,
-    threshold: threshold
-  }
-  
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) { 
-        counter = 0;
-        setData([...data, ...initialData]);
-        observer.unobserve(entry.target)
-      } 
-    })
-  }, options);
-  
-  if (loader && loader.current) {
-    observer.observe(loader.current);
-    return () => {
-      observer.disconnect();
-      lazyobserver.disconnect()
-    };
-  }
-});
+    const options = { // options on props
+      root: rootVal,
+      rootMargin: rootMargin,
+      threshold: threshold
+    }
+    
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) { 
+          console.log('HERE')
+          request().then(res =>  {
+            console.log('INSIDE REQUEST')
+
+            setData((data) => [...data, ...res.hits])
+            pages = pages + 1;
+            return () => {
+              observer.disconnect();
+              lazyobserver.disconnect()
+            };
+          });
+        } 
+      })
+    }, options);
+    
+    if (loader && loader.current) {
+      observer.observe(loader.current);
+     
+    }
+  },[])
+
 const options2 = { // options on props
     root: rootVal,
     rootMargin: '50px',
@@ -55,16 +62,17 @@ const lazyobserver = new IntersectionObserver((entries)=> {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
         lazyobserver.unobserve(entry.target)
-        entry.target.className = "square"
+        entry.target.className = "children"
     }
 })
 }, options2);
-
+ if (!data ) return <h1>Loading</h1>
  return (
   <div className="flex">
-    { data.map ( square => {
+    { data.map ( data => {
         counter = counter +1;
         return <Item 
+          data = {data}
           observer={lazyobserver} 
           key={counter} 
           Children={Children}
@@ -75,98 +83,3 @@ const lazyobserver = new IntersectionObserver((entries)=> {
  }
 
 export default InfiniteScroll;
-
-
-
-// import React, {useRef, useEffect, useState} from 'react';
-// import Item from './lazyLoadItem';
-// import Children from './children';
-
-// let pages = 1
-// const request = async () => {
-//   const response = await fetch('https://pixabay.com/api/?key=12607862-fd9c4e5d2ce0e316e5fe18d32&per_page=50&page=' + pages);
-//   const json = await response.json();
-//   console.log('FETCHED', json);
-  
-//   return(json);
-// }
-
-
-
-
-// let counter = 0;
-// const initialData = Array(50).fill('mock');
-// IntersectionObserver.prototype.POLL_INTERVAL = 100;
-
-// const InfiniteScroll = ( { rootVal = null, rootMargin = '50px', threshold= 1.0 }) => {
-//   const [data, setData] = useState(initialData);
-//   const loader = useRef(null);
-  
-//   useEffect(() => {
-  
-//   async function fetching () {
-//    // request().then(res =>  setData(res.hits));
-//      console.log('entered fetching');
-     
-//   }
-//   console.log('data', data);
-  
-  
-  
-//   const options = { // options on props
-//     root: rootVal,
-//     rootMargin: rootMargin,
-//     threshold: threshold
-//   }
-  
-//   const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) { 
-        
-//         console.log('Entered in view');
-//         counter = 0;
-//         pages = pages + 1;
-//         setData([...data, fetching()]);
-//         observer.unobserve(entry.target)
-//       } 
-//     })
-//   }, options);
-  
-//   if (loader && loader.current) {
-//     observer.observe(loader.current);
-//     return () => {
-//       observer.disconnect();
-//       lazyobserver.disconnect()
-//     };
-//   }
-// }, []);
-// const options2 = { // options on props
-//     root: rootVal,
-//     rootMargin: '50px',
-//     threshold: 0.2
-//   }
-// const lazyobserver = new IntersectionObserver((entries)=> {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//         lazyobserver.unobserve(entry.target)
-//         entry.target.className = "square"
-//     }
-// })
-// }, options2);
-//  if (!data ) return <h1>Loading</h1>
-//  return (
-//   <div className="flex">
-//     { data.map ( data => {
-//         counter = counter +1;
-//         return <Item 
-//           data = {data}
-//           observer={lazyobserver} 
-//           key={counter} 
-//           Children={Children}
-//         />
-//   })}
-//   <div ref={loader}></div>
-//  </div>)
-//  }
-
-// export default InfiniteScroll;
